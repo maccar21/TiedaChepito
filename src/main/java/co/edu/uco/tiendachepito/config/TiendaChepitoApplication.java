bb40bf82b16cfcf8a5bf9e079a2c4a3ac599e90a
Mateo Cardona <teocarias21@gmail.com>
2024-04-26 22:35:36 -0500

    commit inicial

---
+++ src/main/java/co/edu/uco/tiendachepito/config/TiendaChepitoApplication.java
@@ -0,0 +1,13 @@
+package co.udo.uco.tiendachepito.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaChepitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaChepitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/udo/uco/tiendachepito/config/TiendaChepitoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.udo.uco.tiendachepito.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaChepitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
